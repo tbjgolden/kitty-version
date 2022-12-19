f8b6f5a225aaed4ad8a6ff9e8b0f587c3e536602
Tom Golden <oss@tom.bio>
2022-12-19 15:49:25 +0000

    fix: issue with regex causing false negatives

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import { execSync } from "node:child_process";
 let version: string | false | undefined;
 
 const SEMVER_REGEX =
-  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*)(?:\.(?:0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*))*))?(?:\+([\dA-Za-z-]+(?:\.[\dA-Za-z-]+)*))?$/;
+  /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*)(?:\.(?:0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*))*))?(?:\+([\dA-Za-z-]+(?:\.[\dA-Za-z-]+)*))?/;
 
 export const kittyVersion = (): string | undefined => {
   if (process.platform === "win32") {
